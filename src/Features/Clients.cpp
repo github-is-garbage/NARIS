220258f6bf97bb6d78f441442919208dcfabe0f3
github-is-garbage <177191469+github-is-garbage@users.noreply.github.com>
2025-02-13 00:54:14 -0500

    Begin WinSock connection stuff
    Extremely primitive but gotta start somewhere

--- src/Features/Clients.cpp
+++ src/Features/Clients.cpp
@@ -1,19 +1,181 @@
 #include "Clients.h"
 #include "../Globals.h"
 
+#include <stdio.h>
+#include <thread>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+void Clients::CollectClientInformation(SOCKET pSocketClient, ClientSocketInfo_t& SocketInfo)
+{
+	strcpy(SocketInfo.szIdentifier, "Unknown");
+	strcpy(SocketInfo.szAddr, "Unknown");
+	SocketInfo.iPort = 0;
+	SocketInfo.flPing = 0.f;
+	SocketInfo.flConnectionTime = 0.f;
+	SocketInfo.pszOS = "Unknown";
+	SocketInfo.pszInstallDate = "Unknown";
+	SocketInfo.pszVersion = VERSION;
+
+	getpeername(pSocketClient, (sockaddr*)&SocketInfo.ConnectionInfo.ClientAddr, &SocketInfo.ConnectionInfo.ClientAddrSize);
+	inet_ntop(AF_INET, &SocketInfo.ConnectionInfo.ClientAddr.sin_addr, SocketInfo.szAddr, INET_ADDRSTRLEN);
+
+	// TODO: The rest
+}
+
+void ClientHandler(Clients* pClientsFeature, SOCKET pSocketClient, ClientSocketInfo_t SocketInfo)
+{
+	pClientsFeature->CollectClientInformation(pSocketClient, SocketInfo);
+	pClientsFeature->vecClients.emplace_back(SocketInfo);
+
+	closesocket(pSocketClient);
+}
+
+void ListenHandler(SOCKET pSocket)
+{
+	Clients* pClientsFeature = gpGlobals->FeatureManager->FindFeature<Clients>();
+
+	while (true)
+	{
+		sockaddr_in ClientAddr;
+		int ClientAddrSize = sizeof(ClientAddr);
+
+		SOCKET pSocketClient = accept(pSocket, (sockaddr*)&ClientAddr, &ClientAddrSize);
+
+		if (pSocketClient == INVALID_SOCKET)
+		{
+			int iError = WSAGetLastError();
+
+			if (iError == WSAEINTR || iError == WSAENOTSOCK)
+			{
+				MessageBoxW(NULL, L"This should never happen #1", L"", MB_ICONERROR | MB_OK);
+				break;
+			}
+
+			continue;
+		}
+
+		ClientConnectionInfo_t ConnectionInfo;
+		ConnectionInfo.ClientAddr = ClientAddr;
+		ConnectionInfo.ClientAddrSize = ClientAddrSize;
+
+		ClientSocketInfo_t SocketInfo;
+		SocketInfo.ConnectionInfo = ConnectionInfo;
+
+		std::thread(ClientHandler, pClientsFeature, pSocketClient, SocketInfo).detach();
+	}
+}
+
+bool Clients::SetupSocket()
+{
+	WSADATA wsaData;
+	HRESULT Result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+	if (FAILED(Result))
+	{
+		MessageBoxW(NULL, L"Failed to initialize socket!", L"", MB_ICONERROR | MB_OK);
+		return false;
+	}
+
+	addrinfo* pInfo = NULL;
+	addrinfo Hints;
+
+	ZeroMemory(&Hints, sizeof(Hints));
+	Hints.ai_family = AF_INET;
+	Hints.ai_socktype = SOCK_STREAM;
+	Hints.ai_protocol = IPPROTO_TCP;
+	Hints.ai_flags = AI_PASSIVE;
+
+	Result = getaddrinfo(NULL, "27015", &Hints, &pInfo);
+
+	if (FAILED(Result))
+	{
+		WSACleanup();
+
+		MessageBoxW(NULL, L"Failed to get address info!", L"", MB_ICONERROR | MB_OK);
+		return false;
+	}
+
+	SOCKET pSocket = socket(pInfo->ai_family, pInfo->ai_socktype, pInfo->ai_protocol);
+
+	if (pSocket == INVALID_SOCKET)
+	{
+		freeaddrinfo(pInfo);
+		WSACleanup();
+
+		MessageBoxW(NULL, L"Failed to initialize socket!", L"", MB_ICONERROR | MB_OK);
+		return false;
+	}
+
+	Result = bind(pSocket, pInfo->ai_addr, (int)pInfo->ai_addrlen);
+
+	freeaddrinfo(pInfo);
+
+	if (FAILED(Result))
+	{
+		closesocket(pSocket);
+		WSACleanup();
+
+		MessageBoxW(NULL, L"Failed to bind socket!", L"", MB_ICONERROR | MB_OK);
+		return false;
+	}
+
+	Result = listen(pSocket, SOMAXCONN);
+
+	if (FAILED(Result))
+	{
+		closesocket(pSocket);
+		WSACleanup();
+
+		MessageBoxW(NULL, L"Failed to listen on socket!", L"", MB_ICONERROR | MB_OK);
+		return false;
+	}
+
+	std::thread(ListenHandler, pSocket).detach();
+
+	return true;
+}
+
 void Clients::Setup()
 {
+	this->vecClients = std::vector<ClientSocketInfo_t>();
+
+	if (!this->SetupSocket())
+		return exit(-1);
+
 	gpGlobals->GUIManager->AddHook("GUI_ClientList", []()
 	{
-		for (int i = 0; i < 10; ++i)
+		std::vector<ClientSocketInfo_t>& vecClients = gpGlobals->FeatureManager->FindFeature<Clients>()->vecClients;
+
+		for (int i = 0; i < vecClients.size(); ++i)
 		{
+			ClientSocketInfo_t SocketInfo = vecClients[i];
+
 			ImGui::TableNextRow();
 
-			for (int ii = 0; ii < 8; ++ii)
-			{
-				ImGui::TableNextColumn();
-				ImGui::Text("%d %d", i, ii);
-			}
+			ImGui::TableNextColumn();
+			ImGui::Text(SocketInfo.szIdentifier);
+
+			ImGui::TableNextColumn();
+			ImGui::Text(SocketInfo.szAddr);
+
+			ImGui::TableNextColumn();
+			ImGui::Text("%d", SocketInfo.iPort);
+
+			ImGui::TableNextColumn();
+			ImGui::Text("%.0f ms", SocketInfo.flPing * 1000.f);
+
+			ImGui::TableNextColumn();
+			ImGui::Text("%.2f", SocketInfo.flConnectionTime);
+
+			ImGui::TableNextColumn();
+			ImGui::Text(SocketInfo.pszOS);
+
+			ImGui::TableNextColumn();
+			ImGui::Text(SocketInfo.pszInstallDate);
+
+			ImGui::TableNextColumn();
+			ImGui::Text(SocketInfo.pszVersion);
 
 			if (ImGui::TableGetHoveredRow() - 1 == i)
 				ImGui::TableSetBgColor(ImGuiTableBgTarget_RowBg1, ImGui::GetColorU32(ImGuiCol_HeaderHovered));
--- src/Features/Clients.h
+++ src/Features/Clients.h
@@ -2,9 +2,39 @@
 
 #include "../Features.h"
 
+#include <lmcons.h>
+#include <vector>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+struct ClientConnectionInfo_t
+{
+	sockaddr_in ClientAddr;
+	int ClientAddrSize;
+};
+
+struct ClientSocketInfo_t
+{
+	char szIdentifier[UNLEN + CNLEN + 1]; // CN\UN
+	char szAddr[INET_ADDRSTRLEN];
+	int iPort;
+	float flPing;
+	float flConnectionTime;
+	const char* pszOS;
+	const char* pszInstallDate;
+	const char* pszVersion;
+	ClientConnectionInfo_t ConnectionInfo;
+};
+
 class Clients : public BaseFeature
 {
 public:
+	std::vector<ClientSocketInfo_t> vecClients;
+
+public:
+	void CollectClientInformation(SOCKET pSocketClient, ClientSocketInfo_t& SocketInfo);
+	bool SetupSocket();
+
 	void Setup();
 	void Destroy();
 };
